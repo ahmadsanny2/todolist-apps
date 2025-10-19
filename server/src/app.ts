937c53bd8152ab3589994297df4b0dc0efdb3548
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-10-19 20:49:56 +0700

    Testing connect with MongoDB

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,35 @@
 import express, { Request, Response } from "express";
+import mogoose from "mongoose";
+import cors from "cors";
+import mongoose from "mongoose";
+import { error } from "console";
 
 const app = express();
 const PORT = 8080;
 
+const {
+  MONGODB_ATLAS_USERNAME,
+  MONGODB_ATLAS_PASSWORD,
+  MONGODB_ATLAS_DBNAME,
+} = process.env;
+
+const uri = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@cluster0.t62vxtr.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`;
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+app.use(cors());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.info(`App is listening at http://localhost:${PORT}`);
-});
+mogoose.set("useFindAndModify", true);
+mongoose
+  .connect(uri, options)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.info(`App is listening at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    throw error;
+  });
