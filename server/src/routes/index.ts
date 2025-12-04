32d2d4c202c16277478d19df8dd8f23e163492fd
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-12-05 02:28:01 +0700

    Fix routes before testing

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
+import {
+    addTodo,
+    getTodo,
+    getTodos,
+    updateTodo,
+    removeTodo,
+} from "../controllers/todos";
+import bodyParser from "body-parser";
 
-const router = Router()
+const router = Router();
+const jsonParser = bodyParser.json();
 
-router.get('api/todos', getTodos)
+router.get("api/todos", getTodos);
 
-router.get('api/todo/:id', getTodo)
+router.get("api/todo/:id", getTodo);
 
-router.post('api/add-todo', addTodo)
+router.post("api/add-todo", addTodo);
 
-router.put('api/update-todo/:id', updateTodo)
+router.put("api/update-todo/:id", updateTodo);
 
-router.delete('api/remove-todo/:id', removeTodo)
+router.delete("api/remove-todo/:id", removeTodo);
 
-export default router
+export default router;
